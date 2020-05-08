c64a29236613ab59139eff37d4b4c565824e23a1
James Mellish <jameswmellish@gmail.com>
2020-05-08 16:46:32 -0500

    Updates

---
+++ precache-manifest.1ac950045cc95975913a2899165fd2d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d17093a4f65cbb1f2e3d13104a9725",
+    "url": "/rewards-aggregator/index.html"
+  },
+  {
+    "revision": "d2bce983a321b4d96053",
+    "url": "/rewards-aggregator/static/css/main.a22fafb9.chunk.css"
+  },
+  {
+    "revision": "fc7f139d3978ce0b9cd7",
+    "url": "/rewards-aggregator/static/js/2.a9e7da52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rewards-aggregator/static/js/2.a9e7da52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2bce983a321b4d96053",
+    "url": "/rewards-aggregator/static/js/main.1f259bec.chunk.js"
+  },
+  {
+    "revision": "27170e8b59d92b84132f",
+    "url": "/rewards-aggregator/static/js/runtime-main.6929ead7.js"
+  }
+]);
